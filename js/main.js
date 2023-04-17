0d34d4eae818dd6f9ba5fef9b414683c68d00da1
Mohamed Shaheen <91507668+mohamedshaheen26@users.noreply.github.com>
2023-04-17 02:04:32 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+let form = document.getElementById("form"),
+  fName = document.getElementById("fname"),
+  lName = document.getElementById("lname"),
+  email = document.getElementById("email"),
+  password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let firstName = fName.value.trim();
+  let lastName = lName.value.trim();
+  let emailValue = email.value.trim();
+  let passwordValue = password.value.trim();
+  let patternEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (firstName === "") {
+    errorMessage(fName, "First Name Cannot be empty");
+  } else {
+    success(fName);
+  }
+
+  if (lastName === "") {
+    errorMessage(lName, "Last Name Cannot be empty");
+  } else {
+    success(lName);
+  }
+
+  if (emailValue !== "") {
+    if (emailValue.match(patternEmail)) {
+      success(email);
+    } else {
+      errorMessage(email, "Looks Like this is not an email");
+    }
+  } else {
+    errorMessage(email, "Email Cannot be empty");
+  }
+
+  if (passwordValue !== "") {
+    if (passwordValue.length < 8) {
+      errorMessage(password, "Pasword Must be 8 Characters");
+    } else {
+      success(password);
+    }
+  } else {
+    errorMessage(password, "Password Cannot be empty");
+  }
+});
+
+function errorMessage(req, message) {
+  const formControl = req.parentElement;
+  const span = formControl.querySelector("span");
+  span.innerText = message;
+  req.className += "error";
+  span.className += "error-text";
+
+  if (req === email) {
+    req.style.color = "hsl(0, 100%, 74%)";
+  }
+}
+
+function success(req) {
+  req.className += "success";
+}
